e1d9a91e3e9fe7e844d50b18966520ce24e4d55a
hyb1996 <946994919@qq.com>
2017-09-16 12:11:06 +0800

    增加侧拉菜单头像、用户名等位置，增加设置、退出按钮

--- app/src/main/java/com/stareating/nodeet/DrawerFragment.java
+++ app/src/main/java/com/stareating/nodeet/DrawerFragment.java
@@ -18,4 +18,34 @@ public class DrawerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_drawer, container, false);
     }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.exit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().finish();
+            }
+        });
+        view.findViewById(R.id.settings).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: 2017/9/16 跳转到设置
+            }
+        });
+        view.findViewById(R.id.avatar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: 2017/9/16 跳转到用户详情页
+            }
+        });
+        view.findViewById(R.id.header).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: 2017/9/16 让用户设置背景图片 
+            }
+        });
+
+    }
 }
